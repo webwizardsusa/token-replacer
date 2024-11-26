7250b6e18197c99c6cc84fbaa4104676e98c0335
Jamie Holly <jamie@webwizardsusa.com>
2024-11-26 13:46:38 -0500

    Add sample content seed

--- packages/laravel-oembed/src/Exceptions/OembedNotFoundException.php
+++ packages/laravel-oembed/src/Exceptions/OembedNotFoundException.php
@@ -4,7 +4,7 @@ namespace Webwizardsusa\OEmbed\Exceptions;
 
 use Throwable;
 
-class OembedNotFoundException extends OembedException
+class OembedNotFoundException extends OEmbedException
 {
     public function __construct(string $message = "The provider could not determine data for that oembed", int $code = 404, ?Throwable $previous = null)
     {
